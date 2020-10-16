e952d5687a79fe8ef25624bf2b00780799fdecc8
Aiz <dev@aiz.moe>
2020-10-16 16:54:25 +0200

    tabs to spaces

--- scripts/theme-toggle.js
+++ scripts/theme-toggle.js
@@ -8,29 +8,29 @@ let lightTheme = localStorage.getItem('lightTheme');
 const themeToggle = document.querySelector('#theme-toggle');
 
 const enableLightTheme = () => {
-	document.body.classList.add('lightTheme');
-	localStorage.setItem('lightTheme', 'enabled');
-	console.info("Light theme enabled!")
+    document.body.classList.add('lightTheme');
+    localStorage.setItem('lightTheme', 'enabled');
+    console.info("Light theme enabled!")
 }
 
 const disableLightTheme = () => {
-	document.body.classList.remove('lightTheme');
-	localStorage.setItem('lightTheme', null);
-	console.info("Light theme disabled!")
+    document.body.classList.remove('lightTheme');
+    localStorage.setItem('lightTheme', null);
+    console.info("Light theme disabled!")
 }
 
 if (lightTheme === 'enabled') {
-	enableLightTheme();
+    enableLightTheme();
 }
 
 console.log('lightTheme:', localStorage.getItem('lightTheme'));
 
 themeToggle.addEventListener('click', () => {
-	lightTheme = localStorage.getItem('lightTheme');
-	if (lightTheme !== 'enabled') {
-		enableLightTheme();
-	} else {
-		disableLightTheme();
-	}
-	console.log('lightTheme:', localStorage.getItem('lightTheme'));
+    lightTheme = localStorage.getItem('lightTheme');
+    if (lightTheme !== 'enabled') {
+        enableLightTheme();
+    } else {
+        disableLightTheme();
+    }
+    console.log('lightTheme:', localStorage.getItem('lightTheme'));
 });
